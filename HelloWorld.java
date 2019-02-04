e9191c3df8776c3c9f1331ea727bf6ee776c1ddb
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2019-02-04 17:18:04 -0500

    Hello World Dr Java test

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * HelloWorld.java
+ * @version 1.0
+ * @author Allen Liu
+ * @since Feb 4, 2019
+ * A program that prints "Hello, World!" to console.
+ */
+
+public class HelloWorld {
+  
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
